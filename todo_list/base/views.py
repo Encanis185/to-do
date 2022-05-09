aa62aef165290161b830395f2d1ceafcc83da32e
Josafat Jauregui <alfonso.lopez5658@alumnos.udg.mx>
2022-05-09 16:49:02 -0500

    Agregadas las funciones del crud. Solo se puede ver si el usuario esta autentificado

--- todo_list/base/views.py
+++ todo_list/base/views.py
@@ -1,20 +1,55 @@
+from dataclasses import fields
+from pydoc import describe
+from turtle import title
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView
 from django.views.generic.edit import UpdateView
 from django.views.generic.edit import DeleteView
+
+from django.contrib.auth.views import LoginView
+
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+from django.urls import reverse_lazy
+
 from .models import Task
 
 # Create your views here.
 
-class TaskList(ListView):
+class CustomLoginView(LoginView):
+    template_name = 'base/login.html'
+    fields = '__all__'
+    redirect_authenticated_user = True
+
+    def get_success_url(self) -> str:
+        return reverse_lazy('tasks')
+
+
+class TaskList(LoginRequiredMixin,ListView):
     model = Task
     context_object_name = 'tasks'
     template_name = 'base/task_list.html'
 
-class TaskDetail(DetailView):
+class TaskDetail(LoginRequiredMixin, DetailView):
     model = Task
     context_object_name = 'task'
     template_name = 'base/task_detail.html'
+
+class TaskCreate(LoginRequiredMixin, CreateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
+
+class TaskUpdate(LoginRequiredMixin, UpdateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
+
+class TaskDelete(LoginRequiredMixin, DeleteView):
+    model = Task
+    context_object_name = 'task'
+    success_url = reverse_lazy('tasks')
+
 
