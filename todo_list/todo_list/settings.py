9efec04dd661a6fa4353c0c7e74c26ce4004a6ef
Josafat Jauregui <alfonso.lopez5658@alumnos.udg.mx>
2022-05-09 22:04:05 -0500

    Se agrega estilos. Ya casi queda todo, queda pendiente modificar el formato de agregar tarea, agregar imagenes y priorizar

--- todo_list/todo_list/settings.py
+++ todo_list/todo_list/settings.py
@@ -10,7 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
+import mimetypes
+mimetypes.add_type("text/css", ".css", True)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -39,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'base.apps.BaseConfig',
 ]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -117,8 +121,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR,'base\static'),)
 
 LOGIN_URL = 'login/'
+
+DATE_INPUT_FORMATS = ['%d-%m-%Y']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
